e1c1ce6a3e094875a945f721f5f39eb1c816dfbb
hojat <hojat72elect@gmail.com>
2022-01-31 21:01:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a simple kotlin game engine, I'm trying to make it a tool 
+suitable enough for my Android games such as "https://github.com/hojat72elect/Flappy_Bird".
+
+The main tutorial that explained about this engine is in here: "https://youtu.be/025QFeZfeyM".
+
+This engine is in alpha version and not yet ready for usage.
---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+    java
+}
+
+group = "ca.sudbury.hojat"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+val lwjglVersion = "3.3.0"
+val jomlVersion = "1.10.3"
+val lwjglNatives = "natives-windows"
+
+
+
+dependencies {
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-nfd")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-stb")
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-nfd", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+    implementation("org.joml", "joml", jomlVersion)
+}
